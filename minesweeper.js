3be5966ac6d7ed6082572eb012faf2a3afcd256f
francinebc <francinebc.mail@gmail.com>
2018-12-11 11:31:28 +1300

    Added win conditions

--- minesweeper.js
+++ minesweeper.js
@@ -8,14 +8,38 @@ var board = {
 
 
 const rows = 5;
+const mineNo = 9;
 
 function populateCells(){
   // make a cell object for every row, col position on board
   for(var i=0; i<rows; i++){
     for(var j=0; j<rows; j++){
-      board.cells.push({row: i, col: j, isMine: false, hidden: false});
+      board.cells.push({row: i, col: j, isMine: false, hidden: true});
     }
   }
+  // assign mines
+  var isAMine = randomRowCols();
+  for(var i=0; i<isAMine.length; i++){
+    board.cells[isAMine[i][0]*rows+isAMine[i][1]].isMine = true;
+  }
+}
+
+function randomRowCols(){
+  var ranArray = [];
+  for(var i=0; ranArray.length<=mineNo; i++){
+    var row = Math.floor(Math.random()*rows);
+    var col = Math.floor(Math.random()*rows);
+    // find if this value is already in the array
+    var found = ranArray.find(function(pos){
+      return pos[0]===row && pos[1]===col;
+    })
+    // if not in array then add
+    if(found == undefined){
+      ranArray.push([row, col]);
+    }
+    // console.log ("found? " + found + " row: " + row + " col: " + col);
+  }
+  return ranArray;
 }
 
 function startGame () {
@@ -32,6 +56,24 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
+  console.log("I was called");
+  for (var i=0; i<board.cells.length; i++){
+    // if not a mine but hidden - no win
+    if(!board.cells[i].isMine && board.cells[i].hidden){
+      return false;
+    }
+    // if is a mine but mark property does not exist or marked false
+    if(board.cells[i].isMine){
+      if(!board.cells[i].hasOwnProperty("isMarked")){
+        return false;
+      }
+      if(!board.cells[i].isMarked){
+        return false;
+      }
+    }
+  }
+  displayMessage('You win!')
+  return true;
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
